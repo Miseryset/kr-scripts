ccb183a96c8ba52619653ee91ec3379978e238cd
Miseryset <1598654374@qq.com>
2023-01-31 15:07:26 +0800

    添加cacert.pem

--- pio/src/main/assets/scripts/download_from_github.sh
+++ pio/src/main/assets/scripts/download_from_github.sh
@@ -1,14 +1,15 @@
 tag="latest"
 #tag="tags/Prerelease-Alpha"
 
-url=$(curl -s ${1}/${tag} \
+cacert_file="${START_DIR}/kr-script/custom/other/cacert.pem"
+url=$(curl -s --cacert "${cacert_file}" ${1}/${tag} \
 | grep "${2}" \
 | cut -d : -f 2,3 \
 | tr -d \")
 
 url=${url#*https}
 
-curl -L "https${url}" -o ${3}
+curl --cacert "${cacert_file}" -L "https${url}" -o ${3}
 download_path="/sdcard/Download"
 
 if [ -e ${3} ]; then
